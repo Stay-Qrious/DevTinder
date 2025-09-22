f8c4e7b928070a432b6a63f204d5742c81129b59
Abhishek Tripathi <abhishektripathi8582@gmail.com>
2025-09-23 03:48:57 +0530

    routers and middleware

--- src/app.js
+++ src/app.js
@@ -3,6 +3,31 @@ const app = express();
 
 app.listen(3000, () => { console.log("Server started at port 3000"); });
 
-app.use("/", (req, res) => {
-    res.send("Hellll        asd  fa    dfasdv     fllv   vvvl    ll    sjflkasjflk");
+app.use("/", (err,req, res,next) => {
+  console.log("This is error from home page");
 })
+
+app.use("/admin", (err,req, res,next) => {
+    throw new Error("This is error from admin page");
+    if(err){
+        console.log("This is error from admin page");
+        res.status(500).send("Something broke!");
+    }   
+ 
+});
+
+
+app.get("/admin/getData", (req, res) => {
+    res.send("This is absfjkslkjflksjdfklsjdklout page");
+});
+
+app.get("/about/us", (req, res,next) => {
+    console.log("This is khiikhiii us page");
+    res.send("This is about us page");
+    next();
+    console.log("This is about us page");
+},(req, res, next) => {
+    console.log("This is khiikhiii us page - 2");
+    // next();
+});
+
